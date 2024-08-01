59d055f78cd08dbbdb8cfb2e286b1c13817db8db
megatron-78 <mk6454684@gmail.com>
2024-08-01 14:11:02 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 26924168
 api_hash = "b05041459456cb772f25175e88ffdc53"
 bot_token = "7314398181:AAENfxCynPRqJ9WyVKN4otc_dv3u78hNCrw"
-AUTH = 6401497985
+AUTH = 7299627144
 #Bot Username = "adjrynarggbot"
