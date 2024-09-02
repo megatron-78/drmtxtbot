4c13e65b86673fa73e080683adbcabdedf914cd1
megatron-78 <mk6454684@gmail.com>
2024-09-02 13:46:48 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
-api_id = 26924168
-api_hash = "b05041459456cb772f25175e88ffdc53"
-bot_token = "7314398181:AAENfxCynPRqJ9WyVKN4otc_dv3u78hNCrw"
-AUTH = 7299627144
+api_id = 23865844
+api_hash = "644ca71d8f3a8bf718cc317c53114d9d"
+bot_token = "77534254348:AAFDHx0y3hpzt4-7xnN0IEtAN7_b25jAHFE"
+AUTH = 7535195022 6622333718
 #Bot Username = "adjrynarggbot"
